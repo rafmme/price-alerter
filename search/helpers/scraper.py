68e513a81b805755c0b703f66cb422bac7ec2056
Timileyin Farayola <rafmme@gmail.com>
2022-06-02 18:01:38 +0100

    Fix selenium crash

--- search/helpers/scraper.py
+++ search/helpers/scraper.py
@@ -15,6 +15,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--window-size=1920,1200")
+options.add_argument("--disable-dev-shm-usage")
 
 
 def get_webpage(url: str):
